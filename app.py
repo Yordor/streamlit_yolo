bdb87d8f1563a133a00f9d4351d272ad3503c14a
Yordor <136787439+Yordor@users.noreply.github.com>
2023-06-20 19:42:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ if uploaded_file is not None:
   
   st.write("")
   st.write("Detecting...")
-  img_widld= imgRGB.shape[1]
-  center_img=img_widld/2
+  img_widld = imgRGB.shape[1]
+  center_img = img_widld/2
   result = model(imgRGB, size=600)
   
   detect_class = result.pandas().xyxy[0] 
@@ -36,7 +36,7 @@ if uploaded_file is not None:
   #0  148.605362   0.0    1022.523743  818.618286    0.813045      2      turtle
   st.code(detect_class[['name', 'xmin','ymin', 'xmax', 'ymax']])
   
-  detect_left = detect_class[dect_class['student']==center_img]
+  detect_left = detect_class[dect_class['student']=center_img]
   textall= 'เจอนร.ทั้งหมด : '+str(detect_class.shape[0])+'คน'
   textvol= 'เจอนร.บนสนามvolleyทั้งหมด : '+str(detect_left.shape[0])+'คน'
   st.success(textall)
