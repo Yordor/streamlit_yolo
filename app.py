1cae3cb427a34d0b1372f976dce0ee6c1a0c9c75
Yordor <136787439+Yordor@users.noreply.github.com>
2023-06-20 21:06:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("Detection กังๆ")
 st.write("Upload your Image...")
 
 #model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/nimon.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt')
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
   
